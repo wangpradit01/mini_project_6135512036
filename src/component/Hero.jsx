5a4b5da86da6fa71f4d5ce2b50175bff95d9f35b
wangpradit01 <hwangpradisth@gmail.com>
2022-04-18 00:03:52 +0700

    commit

--- src/component/Hero.jsx
+++ src/component/Hero.jsx
@@ -23,16 +23,8 @@ const Hero = () => {
         <div>
             <img className="px-10" src={bgImg} alt="/" />
         </div>
-        {/* <br></br> <br></br> <br></br> <br></br> <br></br> <br></br> <br></br> <br></br> */}
-        <div className=' absolute flex flex-col py-8 md:min-w-[1528px] bottom-[5%] mx-1 md:left-1/2 transfrom md:-translate-x-1/2 bg-orange-500  text-center shadow-xl'>
-      <p className='text-white font-bold text-3 xl'>Recipes</p>
-      <div className="flex justify-between flex-wrap px-4">
-            <p className='text-white'>Icon. App Security</p>
-            <p className='text-white'>Icon. Dashboard Design</p>
-            <p className='text-white'>Icon. Cloud Data</p>
-            <p className='text-white'>Icon. API</p>  
-      </div>
-    </div>
+       
+       
 
       </div>
     </div>
